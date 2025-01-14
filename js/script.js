822762ef85c3736a6189e3201575b2950ca76322
Ivan Setiawan <setwann22@gmail.com>
2025-01-14 18:35:12 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// fungsi memindah halaman
+document.getElementById("about-button").addEventListener("click", function () {
+  window.location.href = "about.html"; // Ganti dengan URL tujuan Anda
+});
